354a55666eb34ae8bff16fe8634ac890fefbbd41
Eugene <yeo.eugene.oey@gmail.com>
2015-04-04 06:29:26 +0800

    Refactor Mesh Simplification

--- cs3242/mesh_simplification/main.py
+++ cs3242/mesh_simplification/main.py
@@ -1,3 +1,4 @@
+import sys
 import itertools
 import numpy as np
 import heapq
@@ -16,7 +17,6 @@ class Vertex(object):
     def __repr__(self):
         return "({}, {}, {})".format(*self.vector)
         
-
 
 class Face(object):
     def __init__(self, a, b, c):
@@ -43,14 +43,10 @@ class Edge(object):
     def computeOptimalVector(self):
         v1, v2 = self.vertex_pair
         self.optimal_vector = 0.5 * (v1.vector + v2.vector)
+        
+    def computeCost(self):
+        v1, v2 = self.vertex_pair
         optimal_vector_matrix = np.matrix(np.append(self.optimal_vector, [1], 1))
-
-        # The error Quadric of this target v becomes the cost of contracting that pair
-        # print("Error Quadric Matrices")
-        # print("v'", optimal_vector_matrix)
-        # print("Q1", v1.Q)
-        # print("Q2", v2.Q)
-        # print("-------------------")
         self.cost = (optimal_vector_matrix * (v1.Q + v2.Q) * optimal_vector_matrix.T).item()
         
     def __repr__(self):
@@ -58,16 +54,12 @@ class Edge(object):
 
 class VertexPairPriorityQueue(object):
 
-    def __init__(self, vertices):
+    def __init__(self):
 
         self.queue = []
         self.entry_finder = {}
         self.counter = itertools.count()
-        for vertex in vertices:
-            for edge in vertex.edges.values():
-                if (edge.vertex_pair not in self.entry_finder):
-                    self.add(edge)
-
+        
     def add(self, edge):
         if (edge.vertex_pair in self.entry_finder):
             self.remove(edge)
@@ -78,37 +70,37 @@ class VertexPairPriorityQueue(object):
         heapq.heappush(self.queue, entry)
 
     def remove(self, edge):
-        try:
-            entry = self.entry_finder.pop(edge.vertex_pair)
-        except KeyError:
-            pass
-        else:
-            entry[-1] = None
+        entry = self.entry_finder.pop(edge.vertex_pair)
+        entry[-1] = None
 
     def pop(self):
         while self.queue:
             cost, count, edge = heapq.heappop(self.queue)
             
             if edge and edge.vertex_pair in self.entry_finder:
-                del self.entry_finder[edge.vertex_pair]
-                return edge
+                if (edge.cost != cost):
+                    self.add(edge)
+                else:
+                    # print(edge.cost)
+                    del self.entry_finder[edge.vertex_pair]
+                    return edge
         raise KeyError('Queue is Empty')
-
-    def empty(self):
-        return False if self.queue else True
 
 
 class Mesh(object):
     def __init__(self):
         self.face_list = []
         self.vertex_list = []
-
-
+        self.pq = VertexPairPriorityQueue()
         
     def load(self, filename):
         with open(filename) as f:
             for line in f:
-                command, *args = line.split()
+                if line.split():
+                    command, *args = line.split()
+                else:
+                    command = ''
+
                 if command == 'v':
                     args = [float(arg) for arg in args]
                     self.vertex_list.append(Vertex(args[0], args[1], args[2]))
@@ -122,7 +114,6 @@ class Mesh(object):
                     a.faces.add(face)
                     b.faces.add(face)
                     c.faces.add(face)
-
 
     def save(self, filename):
         with open(filename, 'w') as f:
@@ -167,7 +158,13 @@ class Mesh(object):
     def _computeContractionTarget(self):
         for vertex in self.vertex_list:
             for edge in vertex.edges.values():
-                edge.computeOptimalVector()
+                self._recomputeEdge(edge)
+
+    def _recomputeEdge(self, edge):
+        edge.computeOptimalVector()
+        edge.computeCost()
+        self.pq.add(edge)
+
 
     def _contract(self, edge):
         """Return v1 and remove v2
@@ -178,13 +175,7 @@ class Mesh(object):
 
         v1, v2 = edge.vertex_pair
         if v1 not in self.vertex_list or v2 not in self.vertex_list:
-            # raise KeyError("Vertex is not in the list")
             return
-
-        # print("v1 E: ", len(v1.edges))
-        # print("v2 E: ", len(v2.edges))
-        # print("v1 F: ", len(v1.faces))
-        # print("v2 F: ", len(v2.faces))
 
         # Remove v2 from vertex_list
         self.vertex_list.remove(v2)
@@ -212,91 +203,72 @@ class Mesh(object):
                 v1.faces.add(face)
 
 
+        # Every Occurance of v2 in all valid pairs is replaced by v1
+        for vertex_pair, v2_edge in v2.edges.items():
+            if v2_edge != edge:
+                vp = frozenset({v1 if v == v2 else v for v in vertex_pair}) 
+                if len(vp) == 2: 
+                    v2_edge.vertex_pair = vp
+                    v1.edges[vertex_pair] = v2_edge
 
-        # every occurance of v2 in valid pairs is replaced by v1, (remove duplicate)
-        affected_vertices = [v for face in v1.faces for v in face.vertices if v != v1 and v != v2]
-        
-        for v in affected_vertices:
-            v.edges[edge.vertex_pair] = edge
-
-        for edge in v1.edges.values():
-            edge.computeOptimalVector()
-            self.pq.add(edge)
-            
-        for edge in v2.edges.values():
-            v1.edges[edge.vertex_pair] = edge
-            self.pq.add(edge)
-
+                    # BUG: Recomputing Edge Cost deform the meshes
+                    # self._recomputeEdge(v2_edge)
         return v1
 
     def simplify(self, face_count):
+
+        print("Original Face count: ", len(self.face_list))
+
         # Step 1: Compute the Q matrices for all the initial vertices.
+        print("initializing Matrices")
         self._initQmatrices()
 
         # Step 2: Select all valid pairs
+        print("Selecting All Edges")
         self._selectValidVertexPair()
 
         # Step 3: Compute the optimal contracti1on target v for each valid pair
+        print("Computing Optimal Collapse Point")
         self._computeContractionTarget()
 
         # Step 4: Place all the pairs in a min-heap keyed on cost
-        self.pq = VertexPairPriorityQueue(self.vertex_list)
-
+        print("Enqueing Vertices to Heap")
+        for vertex in self.vertex_list:
+            for edge in vertex.edges.values():
+                if (edge.vertex_pair not in self.pq.entry_finder):
+                    self.pq.add(edge)
 
         # Step 5: Iteratively remove the pair (v1, v2) of least cost from the heap,
         # contract this pair, and update the costs of all valid pairs involving v1 .
-        while (not self.pq.empty() and len(self.face_list) > face_count):
-            edge = self.pq.pop()
-            v = self._contract(edge)       
-            print("FL", len(self.face_list))
-            print("VL", len(self.vertex_list))
-            print("PQQ", len(self.pq.queue))
-            print("PQE", len(self.pq.entry_finder))
-            print()
+        print("Collapsing Edges")
+        while (len(self.face_list) > face_count):
+            try:
+                edge = self.pq.pop()
+            except KeyError:
+                print("Unable to reduce Further")
+                break
+            else:
+                v = self._contract(edge)
+        else:
+            print("Mesh Simplified Successful")
+        print("Face Reduced to: ", len(self.face_list))
 
-def ball():    
-    mesh = Mesh()
-    mesh.load('data/ball.obj')
-    mesh.simplify(78)
-    mesh.save('data/ball2.obj')
-
-def ball2():    
-    mesh = Mesh()
-    mesh.load('data/ball.obj')
-    mesh.simplify(10)
-    mesh.save('data/ball3.obj')
-
-def cow():
-    mesh = Mesh()
-    mesh.load('data/cow5804.mesh')
-    mesh.simplify(500)
-    mesh.save('data/cow500.mesh')
-
-def sphere():
-    mesh = Mesh()
-    mesh.load('data/sphere.obj')
-    mesh.simplify(18)
-    mesh.save('data/sphere19.obj')
-
-def tetrahedron():
-    mesh = Mesh()
-    mesh.load('data/try.obj')
-    mesh.simplify(5)
-    mesh.save('data/try123.obj')
-
-def plane():
-    mesh = Mesh()
-    mesh.load('data/plane.obj')
-    mesh.simplify(7)
-    mesh.save('data/plane123.obj')
 
 def main():
-    #ball()
-    #ball2()
-    #sphere()
-    cow()
-    #tetrahedron()
-    #plane()
+    print("""
+======================
+  Mesh Simplification 
+======================
+        """)
+    if len(sys.argv) != 4:
+        print("Parameters: <input-file> <output-file> <face-count>")
+    else:
+        print(sys.argv)
+        mesh = Mesh()
+        mesh.load(sys.argv[1])
+        mesh.simplify(int(sys.argv[3]))
+        mesh.save(sys.argv[2])
+
 
 if __name__ == '__main__':
     main()
